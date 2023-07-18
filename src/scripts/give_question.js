8edf02050c37e342d93c8b7573fa81433ed57a92
Nicolas Vero <nicolas.vero@univ-rouen.fr>
2023-07-18 21:49:16 +0200

    amélioration du code

--- src/scripts/give_question.js
+++ src/scripts/give_question.js
@@ -1,26 +1,27 @@
-// give_question.js
 import { charger_json } from './load_json.js';
 
 async function displayQuestions() {
     try {
         const all_questions = await charger_json();
+        const [number, submit, display] = [
+            document.getElementById("number"), 
+            document.getElementById("submit"), 
+            document.getElementById("display")
+        ];
 
-        console.log(all_questions);
-
-        document.getElementById("number").addEventListener('click', () => {
-            document.getElementById("number").value = '';
+        number.addEventListener('click', () => {
+            number.value = '';
         });
 
-        document.getElementById("submit").addEventListener('click', () => {
-            let value = document.getElementById("number").value;
-            console.log(value)
-            if(value > all_questions.length || value < 1 || value === '') {
+        submit.addEventListener('click', () => {
+            let value = number.value;
+
+            if(value > all_questions.length || value < 1 || value === '') 
                 value = Math.floor((Math.random() * all_questions.length) + 1);
-                console.log("Modif val : " + value)
-            }
-
-            document.getElementById("display").innerHTML = all_questions[value - 1];
+            
+            display.innerHTML = all_questions[value - 1];
         });
+        
     } catch (err) {
         console.error("Questions non reçues : " + err);
     }
