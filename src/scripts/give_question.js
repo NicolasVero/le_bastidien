b82b14fcf8a5863e610f5c284046b358fb39b0b6
Nicolas Vero <nicolas.vero@univ-rouen.fr>
2023-07-18 19:22:12 +0200

    ajout tirage question

--- src/scripts/give_question.js
+++ src/scripts/give_question.js
@@ -1,3 +1,21 @@
-document.getElementById("submit").addEventListener('click', () => {
-    console.log("clic")
-})
+// give_question.js
+import { charger_json } from './load_json.js';
+
+async function displayQuestions() {
+    try {
+        const all_questions = await charger_json();
+
+        console.log(all_questions);
+        document.getElementById("submit").addEventListener('click', () => {
+            let value = document.getElementById("number").value;
+            if(value >= all_questions.length)
+                value = Math.floor(Math.random() * all_questions.length);
+
+            document.getElementById("display").innerHTML = all_questions[value];
+        });
+    } catch (err) {
+        console.error("Questions non reçues : " + err);
+    }
+}
+
+displayQuestions();
---
+++ src/scripts/load_json.js
@@ -0,0 +1,18 @@
+export function charger_json() {
+    return fetch('src/datas/questions.json')
+        
+    .then(response => response.json())
+        .then(jsonData => {
+
+            let questions = [];
+
+            for (let i = 0; i < jsonData.questions.length; i++) {
+                questions.push(jsonData.questions[i].texte);
+            }
+            return questions;
+        })
+        .catch(err => {
+            console.error('Erreur lors du chargement ou parsing JSON :', err);
+            throw err;
+        });
+}
