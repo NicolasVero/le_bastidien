8d7b02ffa4738a132c7a4cf6b6b00cbe002cff98
Nicolas Vero <nicolas.vero@univ-rouen.fr>
2023-07-19 01:27:33 +0200

    correction de l'engrenage

--- src/scripts/parameters.js
+++ src/scripts/parameters.js
@@ -1,24 +1,23 @@
 const [gear] = [
     document.querySelector('.gear')
-];
+  ];
 
-gear.addEventListener('click', () => {
-    let rotation = getRotation(gear);
-    console.log(rotation);
+  let rotation = 0; // Initialisez la rotation à 0 degrés
 
-    rotation = (rotation + 30) % 360;
+  gear.addEventListener('click', () => {
+    rotation += 30;
     gear.style.transform = `rotate(${rotation}deg)`;
-    gear.style.transition = ".5s";
-});
+	gear.style.transition = ".5s";
+  });
 
-
-function getRotation(element) {
-	const transformStyle = window.getComputedStyle(element).getPropertyValue('transform');
-	const matrix = transformStyle.match(/^matrix\((.+)\)$/);
+  // Fonction pour extraire l'angle de rotation actuel
+  function getRotation(element) {
+    const transformStyle = window.getComputedStyle(element).getPropertyValue('transform');
+    const matrix = transformStyle.match(/^matrix\((.+)\)$/);
 
     if (matrix) {
-		const values = matrix[1].split(', ');
-		return Math.round(Math.atan2(parseFloat(values[1]), parseFloat(values[0])) * (180 / Math.PI));
+      const values = matrix[1].split(', ');
+      return Math.round(Math.atan2(parseFloat(values[1]), parseFloat(values[0])) * (180 / Math.PI));
     }
 
     return 0;
