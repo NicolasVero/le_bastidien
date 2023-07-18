589c7580f3c29d073d692c43cb4049d05f968a0d
Nicolas Vero <nicolas.vero@univ-rouen.fr>
2023-07-18 18:53:08 +0200

    ajout champ demande question

--- src/scripts/loadJSON.js
+++ src/scripts/loadJSON.js
@@ -4,6 +4,10 @@ function chargerJSON() {
         .then(jsonData => {
 
             console.log(jsonData);
+
+            for(let i = 0; i < jsonData.questions.length; i++) {
+                document.getElementById("display").innerHTML += i + " : " + jsonData.questions[i].texte + "<br>";
+            }
         })
         .catch(err => {
             console.error('Erreur lors du chargement ou parsing JSON :', err);
