43250bb912347dd39e74f945b8168a2de9e38341
Nicolas Vero <nicolas.vero@univ-rouen.fr>
2023-07-19 02:05:51 +0200

    ajout rideau parametres

--- src/scripts/parameters.js
+++ src/scripts/parameters.js
@@ -1,24 +1,17 @@
-const [gear] = [
-    document.querySelector('.gear')
-  ];
+const [gear, parametres] = [
+    document.querySelector('.gear'),
+    document.querySelector('#parametres')
+];
 
-  let rotation = 0; // Initialisez la rotation à 0 degrés
+let rotation = 0;
 
-  gear.addEventListener('click', () => {
-    rotation += 30;
-    gear.style.transform = `rotate(${rotation}deg)`;
+gear.addEventListener('click', () => {
+	rotation += 30;
+	gear.style.transform = `rotate(${rotation}deg)`;
 	gear.style.transition = ".5s";
-  });
 
-  // Fonction pour extraire l'angle de rotation actuel
-  function getRotation(element) {
-    const transformStyle = window.getComputedStyle(element).getPropertyValue('transform');
-    const matrix = transformStyle.match(/^matrix\((.+)\)$/);
-
-    if (matrix) {
-      const values = matrix[1].split(', ');
-      return Math.round(Math.atan2(parseFloat(values[1]), parseFloat(values[0])) * (180 / Math.PI));
-    }
-
-    return 0;
-  }
+	if (parametres.classList.contains("active"))
+		parametres.classList.remove('active');
+	else
+		parametres.classList.add('active');
+});
